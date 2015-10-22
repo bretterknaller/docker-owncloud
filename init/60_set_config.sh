470b2a4c3ba691e6768d614a9d34f4c099d8c884
sparklyballs <sparklyballs@gmail.com>
2015-10-22 02:44:00 +0100

    fix add memory caching  config after reboot

--- init/60_set_config.sh
+++ init/60_set_config.sh
@@ -3,3 +3,8 @@
 [[ ! -f /config/www/owncloud/config/config.php ]] && exit 0
 
 
+if [ $(grep -o "memcache" /config/www/owncloud/config/config.php | wc -w) == "0" ]; then
+sed -i "s#.*);.*#  'memcache.local' => '\\\OC\\\Memcache\\\APC',\n&#" /config/www/owncloud/config/config.php
+else
+exit 0
+fi
