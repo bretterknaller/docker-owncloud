4c833e9b1b9e25e0c291fe5e5e7bc5503c1c077e
sparklyballs <sparklyballs@gmail.com>
2015-11-11 17:51:14 +0000

    tweak init

--- init/60_set_config.sh
+++ init/60_set_config.sh
@@ -2,7 +2,6 @@
 
 [[ ! -f /config/www/owncloud/config/config.php ]] && exit 0
 
-
 if [ $(grep -o "memcache" /config/www/owncloud/config/config.php | wc -w) == "0" ]; then
 sed -i "s#.*);.*#  'memcache.local' => '\\\OC\\\Memcache\\\APC',\n&#" /config/www/owncloud/config/config.php
 else
